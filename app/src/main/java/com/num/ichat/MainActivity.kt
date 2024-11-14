520851f1c8eb6798bc25760874e4ee34d70308d1
ChhoeurnKhuon <Khounchhoeun66@gmail.com>
2024-11-14 14:31:39 +0700

    add login page

---
+++ app/src/main/java/com/num/ichat/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.num.ichat
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.num.ichat.activity.NumberActivity
+import com.num.ichat.adapter.ViewPagerAdapter
+import com.num.ichat.databinding.ActivityMainBinding
+import com.num.ichat.ui.CallFragment
+import com.num.ichat.ui.ChatFragment
+import com.num.ichat.ui.StatusFragment
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    private lateinit var auth : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        val fragmentArrayList = ArrayList<Fragment>()
+
+        fragmentArrayList.add(ChatFragment())
+        fragmentArrayList.add(StatusFragment())
+        fragmentArrayList.add(CallFragment())
+
+        auth = FirebaseAuth.getInstance()
+
+        if(auth.currentUser == null){
+            startActivity(Intent(this, NumberActivity::class.java))
+            finish()
+        }
+
+        val adapter = ViewPagerAdapter(this, supportFragmentManager,fragmentArrayList)
+
+        binding!!.viewPager.adapter = adapter
+        binding!!.tabs.setupWithViewPager(binding!!.viewPager)
+
+    }
+}
